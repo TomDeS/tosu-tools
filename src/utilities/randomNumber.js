225cb04fa5c005560bcf140d37aad5615b3bc431
TomDeS <desuttertom@gmail.com>
2021-03-14 19:21:26 +0100

    updated random

--- src/utilities/randomNumber.js
+++ src/utilities/randomNumber.js
@@ -1,12 +1,10 @@
 // exports.randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-
-exports.randomNumber = (min: number, max: number): number => {
+exports.randomNumber = (min, max) => {
   // Window is not available at build time!
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     const range = max - min
     const int = window.crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32
-    console.log('window.crypto')
     return Math.floor(int * range + min)
-  } 
+  }
 }
