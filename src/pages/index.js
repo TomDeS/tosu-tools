43f21703850b3b596d0f0e24d892d8b09ced06ed
TomDeS <desuttertom@gmail.com>
2021-03-14 20:25:10 +0100

    move bank account to page 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,36 +5,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Race from "../apps/race/race"
 import Crypto from "../apps/crypto/crypto"
-import GenerateBankAccount from "../apps/bankaccount/bankaccount"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
-    <div className="columns">
-      <div className="column">
-        <p>
-          Here are randomly generated bank account numbers. The Dutch bank
-          account number (NL) passes the 11-test and has the bank code of an
-          existing bank. For Belgium, the bank code is a random number and may
-          or may not match an existing bank. All generated bank accounts should
-          pass most tests. If a invalid bank account number was generated,
-          please copy it and log{" "}
-          <a href="https://github.com/TomDeS/tosu-react/issues">an issue</a>.
-        </p>
-      </div>
-      <div className="column">
-        <p>Generated bank account numbers:</p>
-        <ul className="list-none">
-          <li>
-            BE: <GenerateBankAccount countryCode="BE" />
-          </li>
-          <li>
-            NL: <GenerateBankAccount countryCode="NL" />
-          </li>
-        </ul>
-      </div>
-    </div>
 
     <Crypto />
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,35 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import GenerateBankAccount from "../apps/bankaccount/bankaccount"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <div className="columns">
+      <div className="column">
+        <p>
+          Here are randomly generated bank account numbers. The Dutch bank
+          account number (NL) passes the 11-test and has the bank code of an
+          existing bank. For Belgium, the bank code is a random number and may
+          or may not match an existing bank. All generated bank accounts should
+          pass most tests. If a invalid bank account number was generated,
+          please copy it and log{" "}
+          <a href="https://github.com/TomDeS/tosu-react/issues">an issue</a>.
+        </p>
+      </div>
+      <div className="column">
+        <p>Generated bank account numbers:</p>
+        <ul className="list-none">
+          <li>
+            BE: <GenerateBankAccount countryCode="BE" />
+          </li>
+          <li>
+            NL: <GenerateBankAccount countryCode="NL" />
+          </li>
+        </ul>
+      </div>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
